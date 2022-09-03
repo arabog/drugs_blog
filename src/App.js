44856489ad5812175a4cc3b07e5b42f80fcfb2bd
arabog <arababs2015@gmail.com>
2022-09-03 21:32:57 +0100

    modify: header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import drugs from "./db/drug_info.json"
 
 function App() {
   return (
     <div className="App">
-      <h1>DRUGS BLOG</h1>
+      <div className='header'>
+        {/* <h1>DRUGS BLOG</h1> */}
+      </div>
+
+      <div className='container'>
+        <div className='drugs'>
+          {
+            drugs.map(drug => (
+              <div className='drug'>
+
+                <h1> {drug.drugName} </h1>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='sidebar'>
+          Sidebar
+        </div>
+      </div>
     </div>
   );
 }
